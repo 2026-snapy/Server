1d7f941c60798abeeed5f30050469cf85a40db2d
KwonJH614 <madogkwon@gmail.com>
2026-04-07 20:23:35 +0900

    feat: Swagger UI에 JWT Bearer 인증 설정 추가

---
+++ src/main/java/com/gbsw/snapy/global/common/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.gbsw.snapy.global.common;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Snapy API")
+                        .description("BeReal 확장형 일상 기록 SNS")
+                        .version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components().addSecuritySchemes(SECURITY_SCHEME_NAME, bearerScheme));
+    }
+}
